f3a10769f417bd9cd51f7b4da4ea18e48499f73d
Mahmoud Mostafa Shehata <mahmoud@shehta>
2023-10-25 02:07:28 +0300

    devise init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    # Customize the path as needed
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
